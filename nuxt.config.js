70a60af69c93b687b6d596c9abb7afe63b06472b
shunsuke <shun.8000.sf@gmail.com>
2022-04-21 18:35:56 +0900

    [update] Lighthouse対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Nuxt.jsでのお仕事お待ちしております。' },
       { name: 'format-detection', content: 'telephone=no' },
       { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
     ],
